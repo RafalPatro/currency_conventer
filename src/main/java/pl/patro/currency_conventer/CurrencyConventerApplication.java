54d43c3379a1b7365e76998e726d006b7df4df8f
rafalp <rafal.patro@gmail.com>
2019-11-26 23:16:42 +0100

    init commit

---
+++ src/main/java/pl/patro/currency_conventer/CurrencyConventerApplication.java
@@ -0,0 +1,13 @@
+package pl.patro.currency_conventer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConventerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyConventerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/patro/currency_conventer/CurrencyConventerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.patro.currency_conventer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConventerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
